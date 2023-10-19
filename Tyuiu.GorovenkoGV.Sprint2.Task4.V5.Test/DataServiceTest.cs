6190c4c3935ffc5a8c5c38557a2a79c09322b32a
GorovenkoGleb <gorovenkogv@outlook.com>
2023-10-20 00:09:23 +0500

    Task 4

---
+++ Tyuiu.GorovenkoGV.Sprint2.Task4.V5.Test/DataServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.GorovenkoGV.Sprint2.Task4.V5.Lib;
+
+namespace Tyuiu.GorovenkoGV.Sprint2.Task4.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 8;
+            double y = 2;
+            double res = ds.Calculate(x, y);
+            double wait = 34.693;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 6;
+            double y = 2;
+            double res = ds.Calculate(x, y);
+            double wait = 96.5;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.GorovenkoGV.Sprint2.Task4.V5.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GorovenkoGV.Sprint2.Task4.V5.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GorovenkoGV.Sprint2.Task4.V5.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("634b68d7-6765-4c64-9d41-d1aff9479166")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
