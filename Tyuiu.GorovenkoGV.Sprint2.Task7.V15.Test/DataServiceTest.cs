27a39701780d121d5ecd0889b666e8047d2bea3c
GorovenkoGleb <gorovenkogv@outlook.com>
2023-10-20 23:29:35 +0500

    Task 7

---
+++ Tyuiu.GorovenkoGV.Sprint2.Task7.V15.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.GorovenkoGV.Sprint2.Task7.V15.Lib;
+
+namespace Tyuiu.GorovenkoGV.Sprint2.Task7.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+
+            double x = 1.5;
+            double y = 1.5;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+
+
+        }
+    }
+}
