658a2bd61d6988ad1a9097cf21b75daaba75eb5a
GorovenkoGleb <gorovenkogv@outlook.com>
2023-10-20 21:01:06 +0500

    Task5

---
+++ Tyuiu.GorovenkoGV.Sprint2.Task5.V7.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GorovenkoGV.Sprint2.Task5.V7.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GorovenkoGV.Sprint2.Task5.V7.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("045025a3-4ccb-489e-b4fe-38ec03e85c96")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.GorovenkoGV.Sprint2.Task5.V7/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GorovenkoGV.Sprint2.Task5.V7.Lib;
+
+
+namespace Tyuiu.GorovenkoGV.Sprint2.Task5.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Горовенко Г.В. | ПКТб 23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор switch                                                   *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнил: Горовенко Глеб Васильевич | ПКТб-23-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует оператор switch вычисляет        *");
+            Console.WriteLine("* требуемое значение и возвращает результат.                              *");
+            Console.WriteLine("* Условие: С начала 1990 года по некоторый день прошло n месяцев и 2 дня. *");
+            Console.WriteLine("* Определить название месяца (январь, февраль и т. п.) этого дня.         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИССХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите n-ое количество месяцев: ");
+            int numMouth = Convert.ToInt32(Console.ReadLine());
+            int currentMonth;
+            if (numMouth > 12)
+            {
+                currentMonth = numMouth % 12 + 1;
+            }
+            else
+                if (numMouth == 12)
+            {
+                currentMonth = 2;
+            }
+            else
+                currentMonth = numMouth + 1;
+
+            string res;
+
+            res = "Это месяц: " + ds.FindMonthName(1990, currentMonth);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(res);
+
+
+            Console.ReadKey();  
+        }
+    }
+}
