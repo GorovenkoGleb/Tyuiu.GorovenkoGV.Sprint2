988b47e1203f9ddd3d62714fd3d85f7e6e044f7e
GorovenkoGleb <gorovenkogv@outlook.com>
2023-10-20 22:17:39 +0500

    Task 6

---
+++ Tyuiu.GorovenkoGV.Sprint2.Task6.V15.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GorovenkoGV.Sprint2.Task6.V15.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GorovenkoGV.Sprint2.Task6.V15.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7d949aef-6793-433b-9c90-52ae073b714b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.GorovenkoGV.Sprint2.Task6.V15/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GorovenkoGV.Sprint2.Task6.V15.Lib;
+
+namespace Tyuiu.GorovenkoGV.Sprint2.Task6.V15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Горовенко Г.В. | ПКТб 23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Получение результата из switch                                    *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #15                                                             *");
+            Console.WriteLine("* Выполнил: Горовенко Глеб Васильевич | ПКТб-23-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует сокращенную форму записи         *");
+            Console.WriteLine("* оператора switch вычисляет требуемое значение и возвращает результат.   *");
+            Console.WriteLine("* Условие: Дано целое число k (1 <= k <= 365). Определить, каким днем     *");
+            Console.WriteLine("* недели (понедельником, вторником, …, субботой или воскресеньем)         *");
+            Console.WriteLine("* является k-й день не високосного года, в котором 1 января понедельник.  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИССХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите k-й день не високосного года, в котором 1 января понедельник: ");
+            int numDay = Convert.ToInt32(Console.ReadLine());
+            int result;
+            if (1 <= numDay && numDay <= 365)
+            {
+                result = numDay % 7;
+            }
+            else
+            {
+                
+                result = Convert.ToInt32("Нужно вводить число от 1 до 365");
+            }
+
+            string res;
+
+            res = "Это День: " + ds.FindDayName(result);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(res);
+
+
+            Console.ReadKey();
+        }
+    }
+}
